affa81a6681a839e25c1253b947b2cb91c74845f
Wesley Chow <wgesh8@gmail.com>
2020-12-17 20:30:28 -0800

    Finish script, add README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-dark-photo-remover
+# dark-photo-remover
+This Python script takes "dark" photos and moves them into a labelled folder based on OpenCV average RGB channels. It
+ also features an adjustable "darkness" threshold that you can play around with to fit your needs.
+<br/>
+
+##### For Windows:
+This script requires the opencv-python package. I recommend installing it in the general Python interpreter so you
+ can run this script anywhere. Run the following in Command Prompt.
+ 
+ `python -m pip install opencv-python`
+ 
+_(If you have multiple versions of Python, make sure to install the package to whichever one your script will run with)_
--- dark_photo_remover.py
+++ dark_photo_remover.py
@@ -3,19 +3,25 @@ import os
 import shutil
 
 folder_files = os.listdir()  # get list of files in working dir
-new_path = os.path.dirname(__file__) + "/dark_photos"  # set the new file path
+working_dir = os.path.abspath(os.getcwd())  # set working directory
+new_path = os.path.join(working_dir, "dark_photos")  # set new file path
 
 for file in folder_files:  # go through each file
     if os.path.isfile(file):  # check if 'file' is a file (not folder)
         if not file == os.path.basename(__file__):  # check if 'file' is this script
-            image = cv2.imread(file)  # read the file into opencv
-            mean_channels = cv2.mean(image)  # get the average of each RGB channel
+            image = cv2.imread(file)  # read file into opencv
+            mean_channels = cv2.mean(image)  # get average of each RGB channel
             average_total = mean_channels[0] + mean_channels[1] + mean_channels[2]  # get average "brightness" by
-            # adding all the channels together
-            if average_total < 150:  # if the average is lower than a certain value move the file. change this to
-                # adjust how dark you want images to be defined "dark"
-                print(average_total)
-                # shutil.move(os.path.dirname(__file__) + '/' + file, new_path)
+            # adding all channels together
+            print(average_total)  # prints the total so you can adjust parameter accordingly
 
+            if average_total < 280:  # if average lower than certain value, move file. change this to adjust
+                # "darkness" threshold
+
+                if not os.path.exists(os.path.join(working_dir, "dark_photos")):  # check if dark_photos folder
+                    # exists, if not, create it
+                    os.mkdir(os.path.join(working_dir, "dark_photos"))
+
+                shutil.move(os.path.join(working_dir, file), os.path.join(new_path, file))
     else:
         continue
